a16dddef67e88a8eb24eaf20e8424007c01e372d
Ryan Moon <ryan@mdpert.com>
2020-11-13 20:36:48 +0900

    in development Control Option Mgr

--- app/lib/formatter/DateFormatter.js
+++ app/lib/formatter/DateFormatter.js
@@ -6,18 +6,22 @@ sap.ui.define([
 	return {
 
         toDateString: function(oDate){
+            if(!oDate) return "";
             return DateFormat.getInstance().format(oDate, false);
         },
         
         toUTCDateString: function(oDate){
+            if(!oDate) return "";
             return DateFormat.getInstance().format(oDate, true);
         },
 
         toDateTimeString: function(oDate){
+            if(!oDate) return "";
             return DateFormat.getDateTimeInstance().format(oDate, true);
         },
 
         toUTCDateTimeString: function(oDate){
+            if(!oDate) return "";
             return DateFormat.getDateTimeInstance().format(oDate, true);
         }
 
