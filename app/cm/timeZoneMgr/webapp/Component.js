85912b88da5d6527eae6507f17ae9ab85342a83c
LeeJungHun <leeknow>
2020-11-18 05:30:02 +0000

    template #1

--- app/cm/timeZoneMgr/webapp/Component.js
+++ app/cm/timeZoneMgr/webapp/Component.js
@@ -1,30 +1,16 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"cm/timeZoneMgr/model/models"
-], function (UIComponent, Device, models) {
-	"use strict";
+  "sap/ui/core/UIComponent",
+  "sap/ui/Device",
+  "ext/lib/model/models",
+  "ext/lib/controller/ErrorHandler"
+], function (UIComponent, Device, models, ErrorHandler) {
+  "use strict";
 
-	return UIComponent.extend("cm.timeZoneMgr.Component", {
+  return UIComponent.extend("cm.timeZoneMgr.Component", {
 
-		metadata: {
-			manifest: "json"
-		},
+    metadata: {
+      manifest: "json"
+    }
 
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
-		init: function () {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
-
-			// enable routing
-			this.getRouter().initialize();
-
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-		}
-	});
+  });
 });
