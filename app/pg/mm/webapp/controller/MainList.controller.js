48e6464f0c85c1a5681be8e6f71664d358f00627
pangse <pangsezi@gmail.com>
2020-11-30 11:53:22 +0000

    smart table onBeforeRebindTable 작업중

--- app/pg/mm/webapp/controller/MainList.controller.js
+++ app/pg/mm/webapp/controller/MainList.controller.js
@@ -140,28 +140,72 @@ sap.ui.define([
 			var oSmtFilter = this.getView().byId("smartFilterBar");             //smart filter
 			
             var oTenant_id = oSmtFilter.getControlByKey("tenant_id").getSelectedKey();    
-			var oMaterial_description = oSmtFilter.getControlByKey("material_description").getSelectedKey();   
-			var oSupplier_local_name = oSmtFilter.getControlByKey("supplier_local_name").getSelectedKey();    
-       
-			if (oTenant_id.length > 0) {
-				var oTenant_idFilter = new Filter("tenant_id", FilterOperator.EQ, oTenant_id);
-				mBindingParams.filters.push(oTenant_idFilter);
-            }
-            
-			if (oMaterial_description.length > 0) {
-				var ooMaterial_descriptionFilter = new Filter("material_description", FilterOperator.EQ, oMaterial_description);
-				mBindingParams.filters.push(ooMaterial_descriptionFilter);
-			}
+			var oMaterial_description = oSmtFilter.getControlByKey("material_description").getValue();   
+			var oSupplier_local_name = oSmtFilter.getControlByKey("supplier_local_name").getValue();    
 
-			if (oSupplier_local_name.length > 0) {
-				var oSupplier_local_nameFilter = new Filter("supplier_local_name", FilterOperator.EQ, oSupplier_local_name);
-				mBindingParams.filters.push(oSupplier_local_nameFilter);
-			}
+			if (oTenant_id.length > 0) {
+				mBindingParams.filters.push(new Filter("tenant_id", FilterOperator.EQ, oTenant_id));
+            }
+
+            mBindingParams.filters.push(this._getSearchFilters(oMaterial_description, oSupplier_local_name));
+
+			// if (oMaterial_description.length > 0) {
+			// 	var ooMaterial_descriptionFilter = new Filter("material_description", FilterOperator.Contains, oMaterial_description);
+			// 	mBindingParams.filters.push(ooMaterial_descriptionFilter);
+			// }
+
+			// if (oSupplier_local_name.length > 0) {
+			// 	var oSupplier_local_nameFilter = new Filter("supplier_local_name", FilterOperator.Contains, oSupplier_local_name);
+			// 	mBindingParams.filters.push(oSupplier_local_nameFilter);
+            // }
+                   
+			// if (oTenant_id.length > 0) {
+			// 	var oTenant_idFilter = new Filter("tenant_id", FilterOperator.EQ, oTenant_id);
+			// 	mBindingParams.filters.push(oTenant_idFilter);
+            // }
+            
+			// if (oMaterial_description.length > 0) {
+			// 	var ooMaterial_descriptionFilter = new Filter("material_description", FilterOperator.Contains, oMaterial_description);
+			// 	mBindingParams.filters.push(ooMaterial_descriptionFilter);
+			// }
+
+			// if (oSupplier_local_name.length > 0) {
+			// 	var oSupplier_local_nameFilter = new Filter("supplier_local_name", FilterOperator.EQ, oSupplier_local_name);
+			// 	mBindingParams.filters.push(oSupplier_local_nameFilter);
+			// }
 			
 			console.groupEnd();              
 		},
 
+        _getSearchFilters: function(keyWorld1, keyWorld2) {
 
+            if (keyWorld1.length > 0 && keyWorld2.length > 0) {
+                return new Filter({
+                        filters: [
+                        new Filter("material_description", FilterOperator.Contains, keyWorld1),
+                        new Filter("supplier_local_name", FilterOperator.Contains, keyWorld2),
+                        ],
+                        and: false,
+                });
+            } else if(keyWorld1.length > 0 && keyWorld2.length < 1) {
+
+                return new Filter({
+                        filters: [
+                        new Filter("material_description", FilterOperator.Contains, keyWorld1)
+                        ],
+                        and: false,
+                });
+            } else if(keyWorld1.length < 1 && keyWorld2.length > 0) {
+
+                return new Filter({
+                        filters: [
+                        new Filter("supplier_local_name", FilterOperator.Contains, keyWorld1)
+                        ],
+                        and: false,
+                });
+            }
+        
+        },
 		/**
 		 * System Event onAfterRendering
 		 * @private
