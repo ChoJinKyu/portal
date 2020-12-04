b622add21dd8b0d2bd3825c50ece2d0d1f2983a1
hankiho <ghost74j@nate.com>
2020-12-04 05:58:49 +0000

    수정사항 반영

--- srv/src/main/java/lg/sppCap/handlers/dp/DevelopmentReceipt.java
+++ srv/src/main/java/lg/sppCap/handlers/dp/DevelopmentReceipt.java
@@ -4,17 +4,17 @@ import java.util.List;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.springframework.stereotype.Component;
-import com.sap.cds.services.EventContext;
+// import com.sap.cds.services.EventContext;
 import com.sap.cds.services.cds.CdsService;
-import com.sap.cds.services.cds.CdsCreateEventContext;
-import com.sap.cds.services.cds.CdsReadEventContext;
+// import com.sap.cds.services.cds.CdsCreateEventContext;
+// import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.handler.EventHandler;
-import com.sap.cds.services.handler.annotations.On;
+// import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.Before;
-import com.sap.cds.services.handler.annotations.After;
+// import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.ServiceName;
 
-import cds.gen.dp.MoldMst_;
+// import cds.gen.dp.MoldMst_;
 import cds.gen.dp.developmentreceiptservice.*;
 import cds.gen.dp.developmentreceiptservice.MoldMasters_;
 
