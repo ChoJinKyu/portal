5f79b57b92741587ca9abab6658d68d3c1db1f0b
Hyukchan Moon <ryanmoon@cnspartner.com>
2020-12-07 00:00:47 +0000

    Done CodeComboBox

--- app/lib/core/service/ODataV2ServiceProvider.js
+++ app/lib/core/service/ODataV2ServiceProvider.js
@@ -5,7 +5,8 @@ sap.ui.define([
     "use strict";
 
     var serviceUrls = {
-            "cm.util.CommonService": "srv-api/odata/v2/cm.util.CommonService/"
+            "cm.util.CommonService": "srv-api/odata/v2/cm.util.CommonService/",
+            "cm.util.OrgService": "srv-api/odata/v2/cm.util.OrgService/"
         },
         models = {};
     
@@ -13,6 +14,23 @@ sap.ui.define([
         getCommonService: function(sVersion){
             sVersion = sVersion || "v2";
             var sServiceName = "cm.util.CommonService",
+                oModel = models[sServiceName];
+            if(!oModel){
+                oModel = new ODataModel({
+                    serviceUrl: serviceUrls[sServiceName],
+                    defaultBindingMode: "OneTime",
+                    defaultCountMode: "Inline",
+                    refreshAfterChange: false,
+                    useBatch: true
+                });
+                models[sServiceName] = oModel;
+            }
+            return oModel;
+        },
+        
+        getOrgService: function(sVersion){
+            sVersion = sVersion || "v2";
+            var sServiceName = "cm.util.OrgService",
                 oModel = models[sServiceName];
             if(!oModel){
                 oModel = new ODataModel({
