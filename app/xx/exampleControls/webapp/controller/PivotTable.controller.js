88a978a1d501b7798d1c6bb25569878aa52ca5c5
Hyukchan Moon <ryanmoon@cnspartner.com>
2020-12-09 06:38:09 +0000

    Upgrade Pivot example

--- app/xx/exampleControls/webapp/controller/PivotTable.controller.js
+++ app/xx/exampleControls/webapp/controller/PivotTable.controller.js
@@ -3,8 +3,6 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/m/ColumnListItem",
 	"sap/ui/table/Column",
-	"sap/m/Label",
-	"sap/m/Text",
 	"sap/m/MessageBox",
 	"sap/m/MessageToast"
 ], function (Controller, JSONModel, ColumnListItem, Column, Label, Text, MessageBox, MessageToast) {
@@ -13,23 +11,34 @@ sap.ui.define([
 	return Controller.extend("xx.exampleControls.controller.PivotTable", {
 
         onInit: function(){
-            this.setModel(new JSONModel("./mockdata/pivot.json"), "list");
+            var sUrl = sap.ui.require.toUrl("xx/exampleControls/mockdata/pivot.json");
+            this.setModel(new JSONModel(sUrl), "list");
 
             this.oTable = this.byId("table");
             this.oTable.bindAggregation("columns", "list>/columns", function(index, context) {
                 return new Column({
-                    label: new Label({ text: "{list>label}" }),
-                    template: new Text({ text: "{list>" + context.getObject().columnId + "}" })
+                    label: new sap.m.Label({ text: "{list>label}" }),
+                    template: new sap.m.Text({ text: "{list>" + context.getObject().columnId + "}" })
                 });
             });
             this.oTable.bindAggregation("rows", "list>/rows");
         },
 
-        onCheckButtonPress: function(){
-            
-        },
-
-        onTableTestButtonPress: function(){
+        applyStyle: function(){
+            this.oTable = this.byId("table");
+            this.oTable.bindAggregation("columns", "list>/columns", function(index, context) {
+                return new Column({
+                    hAlign: context.getObject().type == "number" ? "Right" : "Center",
+                    label: new sap.m.Label({ text: "{list>label}" }),
+                    template: (function(oColumn){
+                        if(oColumn.type == "boolean"){
+                            return new sap.m.Switch({ state: "{list>" + oColumn.columnId + "}" });
+                        }else{
+                            return new sap.m.Text({ text: "{list>" + oColumn.columnId + "}" });
+                        }
+                    })(context.getObject())
+                });
+            });
         }
 
 	});
