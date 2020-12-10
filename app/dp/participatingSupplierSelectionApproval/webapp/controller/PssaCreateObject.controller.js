750390b1645a37ec1e2cc5a5c3f2f28825a70aa7
ldw1104 <daunlee@mlinkkorea.com>
2020-12-10 00:48:44 +0000

    participating 코드정리작업 진행중

--- app/dp/participatingSupplierSelectionApproval/webapp/controller/PssaCreateObject.controller.js
+++ app/dp/participatingSupplierSelectionApproval/webapp/controller/PssaCreateObject.controller.js
@@ -327,33 +327,115 @@ sap.ui.define([
 			}
 			oViewModel.setProperty("/busy", false);
         },
-        
+
         /**
          * @description Participating Supplier 의 delete 버튼 누를시 
          */
         onPsDelRow : function(){
             var psTable = this.byId("psTable")
-                , psModel = this.getModel("createlist") 
+                , psModel = this.getModel("appDetail") 
                 , oSelected = psTable.getSelectedIndices()
             ;
-            if(oSelected.length > 0){
-                MessageBox.confirm("삭제 하시겠습니까?", {
-                    title : "Comfirmation",
-                    initialFocus : sap.m.MessageBox.Action.CANCEL,
-                    onClose : function(sButton) {
-                        if (sButton === MessageBox.Action.OK) {
-                            oSelected.forEach(function (idx) {
-                                psModel.markRemoved(idx)
-                            });  
-                        };
-                    }.bind(this)
+            if (oSelected.length > 0) {
+                oSelected.forEach(function (idx) {
+                    psModel.markRemoved(idx)
                 });
-
                 psTable.clearSelection();
-            }else{
-                 MessageBox.error("삭제할 목록을 선택해주세요.");
+
+                 console.log("psModel" ,  psModel);
+            } else {
+                MessageBox.error("삭제할 목록을 선택해주세요.");
             }
         },
+        _oFragments: {},
+        onCheck : function(){ console.log("onCheck") },
+        
+        /**
+         * @description file upload 관련 
+         * @date 2020-11-23
+         * @param {*} oEvent 
+         */
+        onChange: function (oEvent) {
+            var oUploadCollection = oEvent.getSource();
+            // Header Token
+            var oCustomerHeaderToken = new UploadCollectionParameter({
+                name: "x-csrf-token",
+                value: "securityTokenFromModel"
+            });
+            oUploadCollection.addHeaderParameter(oCustomerHeaderToken);
+            MessageToast.show("Event change triggered");
+        },
+
+        onFileDeleted: function (oEvent) {
+            MessageToast.show("Event fileDeleted triggered");
+        },
+
+        onFilenameLengthExceed: function (oEvent) {
+            MessageToast.show("Event filenameLengthExceed triggered");
+        },
+
+        onFileSizeExceed: function (oEvent) {
+            MessageToast.show("Event fileSizeExceed triggered");
+        },
+
+        onTypeMissmatch: function (oEvent) {
+            MessageToast.show("Event typeMissmatch triggered");
+        },
+
+        onStartUpload: function (oEvent) {
+            var oUploadCollection = this.byId("UploadCollection");
+            var oTextArea = this.byId("TextArea");
+            var cFiles = oUploadCollection.getItems().length;
+            var uploadInfo = cFiles + " file(s)";
+
+            if (cFiles > 0) {
+                oUploadCollection.upload();
+
+                if (oTextArea.getValue().length === 0) {
+                    uploadInfo = uploadInfo + " without notes";
+                } else {
+                    uploadInfo = uploadInfo + " with notes";
+                }
+
+                MessageToast.show("Method Upload is called (" + uploadInfo + ")");
+                MessageBox.information("Uploaded " + uploadInfo);
+                oTextArea.setValue("");
+            }
+        },
+
+        onBeforeUploadStarts: function (oEvent) {
+            // Header Slug
+            var oCustomerHeaderSlug = new UploadCollectionParameter({
+                name: "slug",
+                value: oEvent.getParameter("fileName")
+            });
+            oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug);
+            setTimeout(function () {
+                MessageToast.show("Event beforeUploadStarts triggered");
+            }, 4000);
+        },
+
+        onUploadComplete: function (oEvent) {
+            var sUploadedFileName = oEvent.getParameter("files")[0].fileName;
+            setTimeout(function () {
+                var oUploadCollection = this.byId("UploadCollection");
+
+                for (var i = 0; i < oUploadCollection.getItems().length; i++) {
+                    if (oUploadCollection.getItems()[i].getFileName() === sUploadedFileName) {
+                        oUploadCollection.removeItem(oUploadCollection.getItems()[i]);
+                        break;
+                    }
+                }
+
+                // delay the success message in order to see other messages before
+                MessageToast.show("Event uploadComplete triggered");
+            }.bind(this), 8000);
+        },
+
+        onSelectChange: function (oEvent) {
+            var oUploadCollection = this.byId("UploadCollection");
+            oUploadCollection.setShowSeparators(oEvent.getParameters().selectedItem.getProperty("key"));
+        },        
         /**
          * @description Participating Supplier 의 Supplier Select 버튼 누를시 나오는 팝업 
          *              , 테이블의 row 가 선택되어 있지 않으면 supplier 세팅 안됨 
@@ -448,95 +530,7 @@ sap.ui.define([
 		onValueHelpCancelPress: function () {
 			this._oSupplierDialog.close();
 		},
-		_oFragments: {},
-        onCheck : function(){ console.log("onCheck") },
-        
-        /**
-         * @description file upload 관련 
-         * @date 2020-11-23
-         * @param {*} oEvent 
-         */
-        onChange: function(oEvent) {
-			var oUploadCollection = oEvent.getSource();
-			// Header Token
-			var oCustomerHeaderToken = new UploadCollectionParameter({
-				name: "x-csrf-token",
-				value: "securityTokenFromModel"
-			});
-			oUploadCollection.addHeaderParameter(oCustomerHeaderToken);
-			MessageToast.show("Event change triggered");
-		},
-
-		onFileDeleted: function(oEvent) {
-			MessageToast.show("Event fileDeleted triggered");
-		},
-
-		onFilenameLengthExceed: function(oEvent) {
-			MessageToast.show("Event filenameLengthExceed triggered");
-		},
-
-		onFileSizeExceed: function(oEvent) {
-			MessageToast.show("Event fileSizeExceed triggered");
-		},
-
-		onTypeMissmatch: function(oEvent) {
-			MessageToast.show("Event typeMissmatch triggered");
-		},
-
-		onStartUpload: function(oEvent) {
-			var oUploadCollection = this.byId("UploadCollection");
-			var oTextArea = this.byId("TextArea");
-			var cFiles = oUploadCollection.getItems().length;
-			var uploadInfo = cFiles + " file(s)";
-
-			if (cFiles > 0) {
-				oUploadCollection.upload();
-
-				if (oTextArea.getValue().length === 0) {
-					uploadInfo = uploadInfo + " without notes";
-				} else {
-					uploadInfo = uploadInfo + " with notes";
-				}
-
-				MessageToast.show("Method Upload is called (" + uploadInfo + ")");
-				MessageBox.information("Uploaded " + uploadInfo);
-				oTextArea.setValue("");
-			}
-		},
-
-		onBeforeUploadStarts: function(oEvent) {
-			// Header Slug
-			var oCustomerHeaderSlug = new UploadCollectionParameter({
-				name: "slug",
-				value: oEvent.getParameter("fileName")
-			});
-			oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug);
-			setTimeout(function() {
-				MessageToast.show("Event beforeUploadStarts triggered");
-			}, 4000);
-		},
-
-		onUploadComplete: function(oEvent) {
-			var sUploadedFileName = oEvent.getParameter("files")[0].fileName;
-			setTimeout(function() {
-				var oUploadCollection = this.byId("UploadCollection");
-
-				for (var i = 0; i < oUploadCollection.getItems().length; i++) {
-					if (oUploadCollection.getItems()[i].getFileName() === sUploadedFileName) {
-						oUploadCollection.removeItem(oUploadCollection.getItems()[i]);
-						break;
-					}
-				}
-
-				// delay the success message in order to see other messages before
-				MessageToast.show("Event uploadComplete triggered");
-			}.bind(this), 8000);
-		},
-
-		onSelectChange: function(oEvent) {
-			var oUploadCollection = this.byId("UploadCollection");
-			oUploadCollection.setShowSeparators(oEvent.getParameters().selectedItem.getProperty("key"));
-        } ,
+		
         /**
          * @description : Popup 창 : 품의서 Participating Supplier 항목의 Add 버튼 클릭
          */
@@ -556,9 +550,12 @@ sap.ui.define([
 				}.bind(this));
             } 
             
-            this._oDialogTableSelect.then(function(oDialog) { 
+           var that = this;
+            this._oDialogTableSelect.then(function (oDialog) {
                 oDialog.open();
-			});
+                that.byId("moldItemSelectionSearch").firePress(); // open 하자마자 조회 하여 보여줌 
+
+            });
         },
         /**
          * @public 
@@ -567,6 +564,46 @@ sap.ui.define([
         onExit: function () {
             this.byId("dialogMolItemSelection").close();
         },
+
+        onMoldItemSelection: function (oEvent) {
+            console.log(oEvent.getParameters());
+            if (oEvent.getParameters().refreshButtonPressed) {
+                this.onRefresh();
+            } else {
+                var aSearchFilters = this._getSearchMoldSelection();
+                this._applyMoldSelection(aSearchFilters); // 로드하자마자 조회 
+            }
+        },
+
+        /**
+        * @description Mold Item Selection Search 조건 리턴  
+        * @param {*} oEvent 
+        */
+        _getSearchMoldSelection: function () {
+            var aSearchFilters = [];
+            // tenant_id  
+            aSearchFilters.push(new Filter("tenant_id", FilterOperator.EQ, 'L1100'));
+            var company = this.byId('MoldItemSearchCompany').mProperties.selectedKey;
+            var plant = this.byId('MoldItemSearchPlant').mProperties.selectedKey;
+            var model = this.byId('moldItemModel').getValue().trim();
+            var partNo = this.byId('moldItemPartNo').getValue().trim();
+
+            if (company != undefined && company != "" && company != null) {
+                aSearchFilters.push(new Filter("company_code", FilterOperator.EQ, company))
+            }
+            if (plant != undefined && plant != "" && plant != null) {
+                aSearchFilters.push(new Filter("org_code", FilterOperator.EQ, plant))
+            }
+            if (model != undefined && model != "" && model != null) {
+                aSearchFilters.push(new Filter("model", FilterOperator.Contains, model))
+            }
+            if (partNo != undefined && partNo != "" && partNo != null) {
+                aSearchFilters.push(new Filter("part_number", FilterOperator.Contains, partNo))
+            }
+
+            return aSearchFilters;
+        },
+
          /**
          * @description  Participating Supplier Fragment Apply 버튼 클릭시 
          */
