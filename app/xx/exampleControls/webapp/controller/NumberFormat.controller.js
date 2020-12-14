456e977d4a23544decaebd99e2cd8f1be474e6fa
Hyukchan Moon <ryanmoon@cnspartner.com>
2020-12-15 00:16:20 +0900

    update some examples

--- app/xx/exampleControls/webapp/controller/NumberFormat.controller.js
+++ app/xx/exampleControls/webapp/controller/NumberFormat.controller.js
@@ -13,47 +13,22 @@ sap.ui.define([
 			var aData = [
 				{
 					expense: "Flight",
-					transactionAmount: {
-						size: 560.67,
-						currency: "EUR"
-					},
-					exchangeRate: 1.00000,
 					amount: 12345670.67
 				},
 				{
 					expense: "Meals",
-					transactionAmount: {
-						size: 180.50,
-						currency: "USD"
-					},
-					exchangeRate: 0.85654,
 					amount: 1234154.72
 				},
 				{
 					expense: "Hotel",
-					transactionAmount: {
-						size: 675.00,
-						currency: "USD"
-					},
-					exchangeRate: 0.85654,
 					amount: 1234578.57
 				},
 				{
 					expense: "Taxi",
-					transactionAmount: {
-						size: 15,
-						currency: "USD"
-					},
-					exchangeRate: 0.85654,
 					amount: 123412.86
 				},
 				{
 					expense: "Daily allowance",
-					transactionAmount: {
-						size: 80.00,
-						currency: "BGN"
-					},
-					exchangeRate: 0.51129,
 					amount: 1234540.90
 				}
 			];
