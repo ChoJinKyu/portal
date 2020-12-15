9513b9f8e5947d924c603cb2e53581bec35fbbc3
ljh <leeknow@hanmail.net>
2020-12-15 23:29:54 +0000

    #menuMgr Complete #2

--- app/cm/menuMgr/webapp/controller/menuMgr.controller.js
+++ app/cm/menuMgr/webapp/controller/menuMgr.controller.js
@@ -63,13 +63,11 @@ sap.ui.define([
                         filters: [
                             // 조회조건
                             new Filter("language_code", FilterOperator.EQ, "KO"),
-                            new Filter("menu_code", FilterOperator.EQ, "CM1100")
+                            new Filter("menu_code", FilterOperator.EQ, "CM1200")
                         ]
                     })
                     // 성공시
                     .then((function (jNodes) {
-                        console.log(">>>>>> results", jNodes);
-                        // Model
                         this.getView().setModel(new JSONModel({
                             "Menu_haa": {
                                 "nodes": jNodes
@@ -78,114 +76,11 @@ sap.ui.define([
                     }).bind(this))
                     // 실패시
                     .catch(function (oError) {
-                        console.log(">>>> failure", oError);
                     })
                     // 모래시계해제
                     .finally((function () {
                         this.getView().setBusy(false);
                     }).bind(this));
-
-
-
-
-
-
-
-
-
-
-
-
-                // this.getView().getModel().read("/Menu_haa", {
-                //     filters: [
-                //         new Filter("language_code", FilterOperator.EQ, "KO"),
-                //         new Filter("menu_code", FilterOperator.EQ, "CM1100")
-                //     ],
-                //     success: (function (oData) {
-                //         var predicates = oData.results
-                //             // PATH를 분리한다.
-                //             .reduce(function (acc, e) {
-                //                 return [...acc, ...((e["path"]).split("/"))];
-                //             }, [])
-                //             // 중복을 제거한다.
-                //             .reduce(function (acc, e) {
-                //                 return acc.includes(e) ? acc : [...acc, e];
-                //             }, [])
-                //             .reduce(function (acc, e) {
-                //                 return [...acc, new Filter({
-                //                   path: 'node_id', operator: FilterOperator.EQ, value1: e
-                //                 })];
-                //             }, []);
-
-                //         this.getView().getModel().read("/Menu_haa", {
-                //             filters: [...predicates],
-                //             success: (function (oData) {
-                //                 var tree = {}, data = oData.results;
-                //                 data.map(function (d) {
-                //                     d["level"] = d.path.split("/").length - 1;
-                //                     d["nodes"] = [];
-                //                     return d;
-                //                 });
-                //                 // 0
-                //                 tree = data.reduce(function (t, d) {
-                //                     if (d.level == 0) t.push(d);
-                //                     return t;
-                //                 }, []);
-                //                 // 1
-                //                 tree = data.reduce(function (t, d) {
-                //                     if (d.level == 1) {
-                //                         t.map(function (t0) {
-                //                             if (t0.node_id == d.parent_id) {
-                //                                 t0.nodes.push(d);
-                //                             }
-                //                             return t0;
-                //                         });
-                //                     }
-                //                     return t;
-                //                 }, JSON.parse(JSON.stringify(tree)));
-                //                 // 2
-                //                 tree = data.reduce(function (t, d) {
-                //                     if (d.level == 2) {
-                //                         t.map(function (t0) {
-                //                             t0.nodes.map(function (t1) {
-                //                                 if (t1.node_id == d.parent_id) {
-                //                                     t1.nodes.push(d);
-                //                                 }
-                //                                 return t1;
-                //                             });
-                //                             return t0;
-                //                         });
-                //                     }
-                //                     return t;
-                //                 }, JSON.parse(JSON.stringify(tree)));
-                //                 // 3
-                //                 tree = data.reduce(function (t, d) {
-                //                     if (d.level == 3) {
-                //                         t.map(function (t0) {
-                //                             t0.nodes.map(function (t1) {
-                //                                 t1.nodes.map(function (t2) {
-                //                                     if (t2.node_id == d.parent_id) {
-                //                                         t2.nodes.push(d);
-                //                                     }
-                //                                     return t2;
-                //                                 });
-                //                                 return t1;
-                //                             });
-                //                             return t0;
-                //                         });
-                //                     }
-                //                     return t;
-                //                 }, JSON.parse(JSON.stringify(tree)));
-                //                 // Model
-                //                 this.getView().setModel(new JSONModel({
-                //                     "Menu_haa": {
-                //                         "nodes": tree
-                //                     }
-                //                 }), "tree");
-                //             }).bind(this)
-                //         })
-                //     }).bind(this)
-                // });
             }
         });
     }
