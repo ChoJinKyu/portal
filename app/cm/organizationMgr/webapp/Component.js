9cbecfc3a13f468c7dc71c662e2fbdfefc4986e1
Dohyung <Dohyung7>
2020-12-15 08:54:40 +0000

    organizationMgr

--- app/cm/organizationMgr/webapp/Component.js
+++ app/cm/organizationMgr/webapp/Component.js
@@ -1,13 +1,23 @@
 sap.ui.define([
-	"ext/lib/UIComponent"
-], function (UIComponent) {
+    "ext/lib/UIComponent",
+    "sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
 	"use strict";
 
 	return UIComponent.extend("cm.organizationMgr.Component", {
 
 		metadata : {
 			manifest: "json"
-		}
+        }
+        // ,
+        //  onInit: function () {
+        //     this.setModel(new JSONModel({
+        //         selectedTabKey: "",
+        //         tenant_id: "",
+        //         company_code: "",
+        //         add: "",
+        //     }), "view");
+        // },
 
 	});
 
