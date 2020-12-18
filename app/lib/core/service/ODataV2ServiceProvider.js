29b6c27b422fd31c1f4e4368489226ae51dd1642
Hyukchan Moon <ryanmoon@cnspartner.com>
2020-12-18 11:20:30 +0900

    fix mutilingual 1000 record problem with ODataXHRService

--- app/lib/core/service/ODataV2ServiceProvider.js
+++ app/lib/core/service/ODataV2ServiceProvider.js
@@ -1,69 +1,26 @@
 sap.ui.define([
-    "sap/ui/base/Object",
+    "./ServiceProvider",
 	"sap/ui/model/odata/v2/ODataModel"
-], function (Object, ODataModel) {
+], function (Parent, ODataModel) {
     "use strict";
 
-    var serviceUrls = {
-            "cm.util.CommonService": "srv-api/odata/v2/cm.util.CommonService/",
-            "cm.util.OrgService": "srv-api/odata/v2/cm.util.OrgService/"
-        },
-        models = {};
+    Parent._createService = function(sParams){
+        return new ODataModel(jQuery.extend({
+                defaultBindingMode: "OneTime",
+                defaultCountMode: "Inline",
+                refreshAfterChange: false,
+                useBatch: true
+            }, sParams || {}));
+    };
+
+    Parent.getCommonService = function(){
+        return this.getService("cm.util.CommonService");
+    };
     
-    return {
+    Parent.getOrgService = function(){
+        return this.getService("cm.util.OrgService");
+    };
 
-        getService: function(serviceName, isNew){
-            if(!serviceUrls[serviceName])
-                throw new Exception("No service registered : " + serviceName);
-            if(isNew === true){
-                return this._createModel({
-                    serviceUrl: serviceUrls[serviceName]
-                });
-            }else{
-                var oModel = models[serviceName];
-                if(!oModel){
-                    oModel = this._createModel({
-                        serviceUrl: serviceUrls[serviceName]
-                    });
-                    models[serviceName] = oModel;
-                }
-                return oModel;
-            }
-        },
-
-        getServiceByUrl: function(serviceUrl, isNew){
-            var oUrl, sServiceName;
-            for(oUrl in serviceUrls){
-                if(serviceUrl == serviceUrls[oUrl]){
-                    sServiceName = oUrl;
-                    break;
-                }
-            }
-            if(sServiceName){
-                return this.getService(sServiceName, isNew);
-            }else{
-                return this._createModel({
-                    serviceUrl: serviceUrl
-                });
-            }
-        },
-
-        _createModel: function(sParams){
-            return new ODataModel(jQuery.extend({
-                    defaultBindingMode: "OneTime",
-                    defaultCountMode: "Inline",
-                    refreshAfterChange: false,
-                    useBatch: true
-                }, sParams || {}));
-        },
-
-        getCommonService: function(){
-            return this.getService("cm.util.CommonService");
-        },
-        
-        getOrgService: function(){
-            return this.getService("cm.util.OrgService");
-        }
-    }
+    return Parent;
 
 });
