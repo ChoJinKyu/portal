2e1f5079e9df6f531fa6fa308a4facb8ab9ada8e
Eun Jeong Bae <baedol@nate.com>
2020-12-21 05:22:41 +0000

    commit

--- app/dp/md/moldApprovalList/webapp/controller/ApprovalObject.controller.js
+++ app/dp/md/moldApprovalList/webapp/controller/ApprovalObject.controller.js
@@ -62,7 +62,7 @@ sap.ui.define([
 
             this.getView().setModel(new ManagedModel(), "company");
             this.getView().setModel(new ManagedModel(), "plant");
-            this.getView().setModel(new ManagedListModel(), "appType");
+            this.getView().setModel(new ManagedModel(), "appType");
 
             this.getView().setModel(new JSONModel(Device), "device"); // file upload 
 
@@ -230,15 +230,14 @@ sap.ui.define([
                 }
             });
 
-            this.appModel = this.getModel("appType");
-
-            this.appModel.addRecord({
-                "title": "Purchase Order Item",
-                "fragment_name": "PurchaseOrderItem",
-                "approver_type_code": "V"
-            }, "/AppType");
-
-            this._showFormFragment();
+            var appModel = this.getModel("appType");
+            appModel.setTransactionModel(this.getModel("util"));
+            appModel.read("/CodeDetails(tenant_id='" + this.tenant_id + "',group_code='DP_MD_APPROVAL_TYPE',code='" + this.approval_type_code + "')", {
+                filters: [],
+                success: function (oData) {
+                    this._showFormFragment(oData.parent_code);
+                }.bind(this)
+            });
 
             if (this.approval_number === "New") {
                 this._onApproverAddRow(0);
@@ -248,13 +247,13 @@ sap.ui.define([
         },
 
         _oFragments: {},
-        _showFormFragment: function () {
+        _showFormFragment: function (fragmentName) {
             var oPageSection = this.byId("pageSection");
             oPageSection.removeAllBlocks();
 
-            this._loadFragment(this.appModel.getData().AppType[0].fragment_name, function (oFragment) {
+            this._loadFragment(fragmentName, function (oFragment) {
                 oPageSection.addBlock(oFragment);
-            })
+            }.bind(this))
 
         },
 
@@ -309,7 +308,7 @@ sap.ui.define([
             if (!this._oFragments[sFragmentName]) {
                 Fragment.load({
                     id: this.getView().getId(),
-                    name: "dp.md.orderApprovalLocal.view." + sFragmentName,
+                    name: "dp.md.moldApprovalList.view." + sFragmentName,
                     controller: this
                 }).then(function (oFragment) {
                     this._oFragments[sFragmentName] = oFragment;
