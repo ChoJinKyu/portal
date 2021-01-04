7ca5fd02e60281163ad1673aa48634b27f7cf6d4
jinseonlee82 <jslee@mlinkkorea.com>
2021-01-04 07:03:19 +0000

    상세 조회 수정

--- app/dp/md/moldApprovalList/webapp/controller/BudgetExecutionApproval.controller.js
+++ app/dp/md/moldApprovalList/webapp/controller/BudgetExecutionApproval.controller.js
@@ -79,11 +79,9 @@ sap.ui.define([
             var schFilter = [];
             var that = this;
             if (this.approval_number == "New") {
-                // ApprovalBaseController.prototype.onInit.call(this);
 
-                this._budgetEditFragment();
             } else {
-                this._budgetViewFragment();
+
                 schFilter = [new Filter("approval_number", FilterOperator.EQ, this.approval_number)
                     , new Filter("tenant_id", FilterOperator.EQ, 'L1100')
                 ];
@@ -104,7 +102,13 @@ sap.ui.define([
                     console.log("md >>>>>>", md); 
                     that._bindComboPlant(oData.results[0].import_company_code);
                 });
-            }  
+            }
+            
+            if(this.getView().getModel("mode").getProperty("/editFlag")){
+               this._budgetEditFragment();
+            }else{
+               this._budgetViewFragment();
+            }
         },
         _bindViewBudget : function (sObjectPath, sModel, aFilter, callback) { 
                 var oView = this.getView(),
@@ -252,15 +256,8 @@ sap.ui.define([
             console.log();
         },
 
-        onPageEditButtonPress1: function () {
-            this._budgetEditFragment();
-            this._toEditMode();
-        },
-
-        onPageCancelButtonPress: function () {
-            this._budgetViewFragment();
-            this._toShowMode();
-        },
+        _toEditModeEachApproval : function(){ this._budgetEditFragment() } ,
+        _toShowModeEachApproval : function(){ this._budgetViewFragment() } ,
 
         _budgetEditFragment : function(){
             console.log("_budgetEditFragment");
