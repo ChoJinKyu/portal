f9613c94d95ee0f514800514c69c7f30cf7ff996
ldw1104 <daunlee@mlinkkorea.com>
2021-01-05 23:53:32 +0000

    preview팝업 코드수정

--- app/dp/md/moldApprovalList/webapp/controller/ParticipatingSupplierSelection.controller.js
+++ app/dp/md/moldApprovalList/webapp/controller/ParticipatingSupplierSelection.controller.js
@@ -330,6 +330,18 @@ sap.ui.define([
             }
             
             console.log("approverPreview " , this.getModel("approverPreview").getData());
+
+            var ref = this.getModel("referer");
+            this.getView().setModel(new ManagedModel(), "refererPreview");
+
+            var rArr = [];
+            if(ref.getData().Referers != undefined && ref.getData().Referers.length >0){
+                ref.getData().Referers.forEach(function(item){
+                    rArr.push(item.referer_empno); 
+                });
+            }
+            this.getModel("refererPreview").setProperty("/refArr", rArr);
+
             var oView = this.getView();
 
             if (!this._oDialogPreview) {
