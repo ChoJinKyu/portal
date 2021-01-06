d6bc0186f891978ba7e4693a492cb945f5f3ee19
ldw1104 <daunlee@mlinkkorea.com>
2021-01-06 00:52:49 +0000

    동적인 currency comboBox

--- app/dp/md/moldApprovalList/webapp/controller/ParticipatingSupplierSelection.controller.js
+++ app/dp/md/moldApprovalList/webapp/controller/ParticipatingSupplierSelection.controller.js
@@ -88,19 +88,19 @@ sap.ui.define([
                     , new Filter("tenant_id", FilterOperator.EQ, 'L1100')
                 ];
 
-                schFilter2 = [
-                    new Filter("tenant_id", FilterOperator.EQ, 'L1100' ),
-                    new Filter("group_code", FilterOperator.EQ, 'DP_MD_LOCAL_CURRENCY' ),
-                    new Filter("language_cd", FilterOperator.EQ, 'KO' ),
-                    new Filter("org_code", FilterOperator.EQ, this.company_code)
-                ]; 
+                // schFilter2 = [
+                //     new Filter("tenant_id", FilterOperator.EQ, 'L1100' ),
+                //     new Filter("group_code", FilterOperator.EQ, 'DP_MD_LOCAL_CURRENCY' ),
+                //     new Filter("language_cd", FilterOperator.EQ, 'KO' ),
+                //     new Filter("org_code", FilterOperator.EQ, this.org_code)
+                // ]; 
 
                 this._bindViewParticipating("/ParticipatingSupplier", "mdItemMaster", schFilter, function (oData) {
                     console.log("ParticipatingSupplier >>>>>>", oData);
                 });
-                this._bindViewCurrency("/OrgCodeLanguages", "psOrgCode", schFilter2, function (oData) {
-                    console.log("OrgCodeLanguages >>>>>>", oData);
-                });
+                // this._bindViewCurrency("/OrgCodeLanguages", "psOrgCode", schFilter2, function (oData) {
+                //     console.log("OrgCodeLanguages >>>>>>", oData);
+                // });
             }  
         },
 
@@ -235,6 +235,16 @@ sap.ui.define([
                 oModel = this.getModel("mdItemMaster"),
                 mstModel = this.getModel("appMaster");
             ;
+            var schFilter2 = [];
+            schFilter2 = [
+                    new Filter("tenant_id", FilterOperator.EQ, 'L1100' ),
+                    new Filter("group_code", FilterOperator.EQ, 'DP_MD_LOCAL_CURRENCY' ),
+                    new Filter("language_cd", FilterOperator.EQ, 'KO' ),
+                    new Filter("org_code", FilterOperator.EQ, data.company_code)
+            ]; 
+            this._bindViewCurrency("/OrgCodeLanguages", "psOrgCode", schFilter2, function (oData) {
+                    console.log("OrgCodeLanguages >>>>>>", oData);
+            });
             console.log(data);
             /** add record 시 저장할 model 과 다른 컬럼이 있을 경우 submit 안됨 */
             var approval_number = mstModel.approval_number;
