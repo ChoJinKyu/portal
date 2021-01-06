3e96a008eb07e8064f3b65e2a4bb9c11db4fc0ff
dh.kim <dh.kim@lgcns.com>
2021-01-06 04:22:48 +0000

    솔루션화 샘플코드

---
+++ srv/src/main/java/lg/sppCap/solutionized/BizRuleInfoService.java
@@ -0,0 +1,45 @@
+package lg.sppCap.solutionized.model;
+ 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+ 
+import lg.sppCap.solutionized.model.BizRuleInfo;
+
+public class BizRuleInfoService {
+ 
+  public BizRuleInfo retrieveBizRuleInfo(String tenantId, String bizRuleId, String altFlag){
+ 
+    try {
+ 
+      BizRuleInfo info = new BizRuleInfo();
+      info.setTenantId(tenantId);
+      info.setBizRuleId(bizRuleId);
+      info.setAltFlag(altFlag);
+        
+      /*
+      Connection conn = DataSourceConnection.getInstance().getConnection();
+ 
+      PreparedStatement stmt = conn.prepareStatement("SELECT TENANT_ID, BIZRULE_ID, ALT_FLG, CALL_TYPE, CALL_HOST, CALL_INFO FROM BIZRULE_INFO where TENANT_ID=? and BIZRULE_ID=? and ALT_FLG=?");
+      stmt.setString(1, info.getTenantId());
+      stmt.setString(2, info.getBizRuleId());
+      stmt.setString(3, info.getAltFlag());
+ 
+      ResultSet rs = stmt.executeQuery("select user_id, user_nm from internal_user");
+ 
+      while (rs.next()) {
+        info.setTenantId(rs.getString(1));
+        info.setBizRuleId(rs.getString(2));
+        info.setAltFlag(rs.getString(3));
+        info.setCallType(rs.getString(4));
+        info.setCallHost(rs.getString(5));
+        info.setCallInfo(rs.getString(6));
+      }
+      */
+      return info;
+    } catch (Exception e) {
+      throw new RuntimeException("Error Occurred while retrieve BizRuleInfo from DB : " + tenantId + ", " + bizRuleId + ", " + altFlag, e);
+    }
+ 
+  }
+}
