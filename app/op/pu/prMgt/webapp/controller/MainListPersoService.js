9fcf0cf0a671c5a84c984b3367ca23aca61b4298
titi112 <titi1122@gmail.com>
2021-01-07 02:21:03 +0000

    op-pu-pr - app

--- app/op/pu/prMgt/webapp/controller/MainListPersoService.js
+++ app/op/pu/prMgt/webapp/controller/MainListPersoService.js
@@ -1,60 +1,89 @@
 sap.ui.define(["jquery.sap.global"],
     function (jQuery) {
         "use strict";
+        var i = 1;
 
         var _columns = [
             {
-                id: "prMgt-mainList-mainColumnCode",
+                id: "prMgt-mainList-mainColumnPR_NUMBER",
                 order: 0,
                 text: "Code",
                 visible: true
             },
             {
-                id: "prMgt-mainList-mainColumnName",
+                id: "prMgt-mainList-mainColumnERP_PR_NUMBER",
                 order: 1,
                 text: "Name",
                 visible: false
             },
             {
-                id: "prMgt-mainList-mainColumnStartDate",
+                id: "prMgt-mainList-mainColumnPR_DESC",
                 order: 2,
                 text: "Start Date",
                 visible: false
             },
             {
-                id: "prMgt-mainList-mainColumnEndDate",
+                id: "prMgt-mainList-mainColumnPR_TYPE_CODE",
                 order: 3,
                 text: "End Date",
                 visible: true
             },
             {
-                id: "prMgt-mainList-mainColumnSiteFlag",
+                id: "prMgt-mainList-mainColumnPR_TYPE_CODE_2",
                 order: 4,
                 text: "Site",
                 visible: true
             },
             {
-                id: "prMgt-mainList-mainColumnCompanyFlag",
+                id: "prMgt-mainList-mainColumnPR_TYPE_CODE_3",
                 order: 5,
                 text: "Company",
                 visible: true
             },
             {
-                id: "prMgt-mainList-mainColumnRoleFlag",
+                id: "prMgt-mainList-mainColumnPR_TEMPLATE_NUMBER",
                 order: 5,
                 text: "Role",
                 visible: true
             },
             {
-                id: "prMgt-mainList-mainColumnOrganizationFlag",
+                id: "prMgt-mainList-mainColumnREQUESTOR_DEPARTMENT_NAME",
                 order: 6,
                 text: "Organization",
                 visible: true
             },
             {
-                id: "prMgt-mainList-mainColumnUserFlag",
+                id: "prMgt-mainList-mainColumnREQUESTOR_NAME",
                 order: 7,
                 text: "User",
+                visible: true
+            }
+            ,
+            {
+                id: "prMgt-mainList-mainColumnREQUESTOR_NAME",
+                order: 7,
+                text: "User1",
+                visible: true
+            }
+            ,
+            {
+                id: "prMgt-mainList-mainColumnPURCHASE_REQUISITION",
+                order: 7,
+                text: "User2",
+                visible: true
+            }
+            ,
+            {
+                id: "prMgt-mainList-mainColumnPR_COUNT",
+                order: 7,
+                text: "User3",
+                visible: true
+            }
+             ,
+            {
+                id: "prMgt-mainList-mainColumnPR_CREATE_STATUS_CODE",
+                order: 7,
+                text: "User4",
                 visible: true
             }
         ];
