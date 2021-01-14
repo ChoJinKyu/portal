9f34d619a154e721c06d459e8f131385d6b437e5
lsh9226 <lsh9226github.com>
2021-01-14 06:26:42 +0000

    McstProjectMgtServiceV4 호출

--- srv/src/main/java/lg/sppCap/handlers/dp/tc/McstProjectMgtServiceV4.java
+++ srv/src/main/java/lg/sppCap/handlers/dp/tc/McstProjectMgtServiceV4.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
+import java.sql.Types;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -24,6 +25,7 @@ import org.springframework.jdbc.core.CallableStatementCreator;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.SqlReturnResultSet;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SqlOutParameter;
 
 import com.sap.cds.reflect.CdsModel;
 import com.sap.cds.services.EventContext;
@@ -39,6 +41,10 @@ import com.sap.cds.services.request.ParameterInfo;
 
 import cds.gen.dp.mcstprojectmgtv4service.*;
 
+/**
+ *  프로시저 호출시 OutPut의 경우 문자열로 적용시 데이터을 받지 못하고 있음 
+ *  OutPut에 경우 O_TABLE형식으로 보내줘야함   
+ */
 @Component
 @ServiceName(McstProjectMgtV4Service_.CDS_NAME)
 public class McstProjectMgtServiceV4 implements EventHandler {
@@ -60,89 +66,55 @@ public class McstProjectMgtServiceV4 implements EventHandler {
         //jdbc.execute(v_sql_commitOption);
                     
         StringBuffer v_sql_callProc = new StringBuffer();
-        v_sql_callProc.append("CALL DP_TC_CREATE_MCST_PROJECT_PROC (");
-        v_sql_callProc.append(" I_TENANT_ID => ?, ");
-        v_sql_callProc.append(" I_PROJECT_CODE => ?, ");
-        v_sql_callProc.append(" I_MODEL_CODE => ?, ");
-        v_sql_callProc.append(" I_MCST_CODE => ?, ");
-        v_sql_callProc.append(" I_USER_ID => ?, ");
-        v_sql_callProc.append(" O_VERSION_NUMBER => ?, ");
-        v_sql_callProc.append(" O_RETURN_CODE => ?, ");
-        v_sql_callProc.append(" O_RETURN_MSG => ? )");  
-             
-        String o_version_number = "";
-        String o_return_code = "";
-        String o_return_msg = "";
+        // v_sql_callProc.append("CALL DP_TC_CREATE_MCST_PROJECT_PROC (");
+        // v_sql_callProc.append(" I_TENANT_ID => ?, ");
+        // v_sql_callProc.append(" I_PROJECT_CODE => ?, ");
+        // v_sql_callProc.append(" I_MODEL_CODE => ?, ");
+        // v_sql_callProc.append(" I_MCST_CODE => ?, ");
+        // v_sql_callProc.append(" I_USER_ID => ?, ");
+        // v_sql_callProc.append(" O_VERSION_NUMBER => ?, ");
+        // v_sql_callProc.append(" O_RETURN_CODE => ?, ");
+        // v_sql_callProc.append(" O_RETURN_MSG => ? ");  
+        // v_sql_callProc.append(" )");  
+
+
+        v_sql_callProc.append("CALL test03 (");
+        v_sql_callProc.append(" O_TABLE => ? ");  
+        v_sql_callProc.append(" )"); 
         
-        log.info("getTenantId : " + context.getInputData().getTenantId());        
-        log.info("getProjectCode : " + context.getInputData().getProjectCode());
-        log.info("getModelCode : " + context.getInputData().getModelCode());
-        log.info("getMcstCode : " + context.getInputData().getMcstCode());
-        log.info("getUserId : " + context.getInputData().getUserId());
-
-    
-        jdbc.update(v_sql_callProc.toString()
-                , context.getInputData().getTenantId()
-                , context.getInputData().getProjectCode()
-                , context.getInputData().getModelCode()
-                , context.getInputData().getMcstCode()
-                , context.getInputData().getUserId()
-                , o_version_number
-                , o_return_code
-                , o_return_msg
-                );
-
-        // SqlReturnResultSet oDTable = new SqlReturnResultSet("O_VERSION_NUMBER", new RowMapper<OutType>(){
-        //     @Override
-        //     public OutType mapRow(ResultSet v_rs, int rowNum) throws SQLException {
-        //         OutType v_row = OutType.create();
-        //         v_row.setReturncode(v_rs.getString("returncode"));
-        //         v_row.setReturnmessage(v_rs.getString("returnmessage"));
-        //         v_row.setSavedkey(v_rs.getString("savedkey"));
-        //         v_result.add(v_row);
-        //         return v_row;
-        //     }
-        // });  
-
-
-        // Object<String> paramList = new ArrayList<String>();
-        // paramList.add(o_version_number);
-        // paramList.add(o_return_code);
-        // paramList.add(o_return_msg);
-
-
-        // Map<String, Object> resultMap = jdbc.call(new CallableStatementCreator() {
-        //     @Override
-        //     public CallableStatement createCallableStatement(Connection con) throws SQLException {
-        //         CallableStatement stmt = con.prepareCall(v_sql_callProc.toString());
-        //         stmt.setString(1, context.getInputData().getTenantId());
-        //         stmt.setString(2, context.getInputData().getProjectCode());
-        //         stmt.setString(3, context.getInputData().getModelCode());
-        //         stmt.setString(4, context.getInputData().getTenantId());
-        //         stmt.setString(5, context.getInputData().getUserId());
-        //         return stmt;
-        //     }
-        // }, paramList);       
-
-
-        // for(int i=0; i<1; i++){
-        //     log.info(paramList.getString(1));
-        //     log.info(paramList.getString("o_version_number"));
-        //     log.info(paramList.getString("o_return_code"));
-        //     log.info(paramList.getString("o_return_msg"));
-        // }
         
-        log.info("o_version_number : " + o_version_number);
-        log.info("o_version_number : " + o_return_code);
-        log.info("o_version_number : " + o_return_msg);
+        CreatePjtOutputData v_result = CreatePjtOutputData.create();
+        // Procedure Call
+        SqlReturnResultSet oTable = new SqlReturnResultSet("O_TABLE", new RowMapper<CreatePjtOutputData>(){
+            @Override
+            public CreatePjtOutputData mapRow(ResultSet v_rs, int rowNum) throws SQLException {
+                v_result.setVersionNumber(v_rs.getString("version_number"));
+                v_result.setReturnCode(v_rs.getString("return_code"));
+                v_result.setReturnMsg(v_rs.getString("return_msg"));
+                return v_result;
+            }
+        });  
 
 
-         CreatePjtOutputData v_row = CreatePjtOutputData.create();
-         v_row.setVersionNumber("11");
-         v_row.setReturnCode("22");
-         v_row.setReturnMsg("33");
-         context.setResult(v_row);
-         context.setCompleted(); 
+        List<SqlParameter> paramList = new ArrayList<SqlParameter>();
+        paramList.add(oTable);        
+
+        Map<String, Object> resultMap = jdbc.call(new CallableStatementCreator() {
+            @Override
+            public CallableStatement createCallableStatement(Connection con) throws SQLException {
+                CallableStatement stmt = con.prepareCall(v_sql_callProc.toString());
+                // stmt.setString("I_TENANT_ID", context.getInputData().getTenantId());
+                // stmt.setString("I_PROJECT_CODE", context.getInputData().getProjectCode());
+                // stmt.setString("I_MODEL_CODE", context.getInputData().getModelCode());
+                // stmt.setString("I_MCST_CODE", context.getInputData().getTenantId());
+                // stmt.setString("I_USER_ID", context.getInputData().getUserId());
+                return stmt;
+            }
+        }, paramList);       
+      
+
+        context.setResult(v_result);
+        context.setCompleted();
 
         log.info("### DP_TC_CREATE_MCST_PROJECT_PROC 프로시저 호출 종료 end ###");
     }
