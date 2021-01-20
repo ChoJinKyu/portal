6b52ddb39beea4783e1af57ef9fa70109611607f
67837 <ultrakim@lgcns.com>
2021-01-20 07:38:26 +0000

    컴포넌트 수정

--- app/dp/md/moldApprovalList/webapp/Component.js
+++ app/dp/md/moldApprovalList/webapp/Component.js
@@ -24,7 +24,7 @@ sap.ui.define([
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// initialize the error handler with the component
-			this._oErrorHandler = new ErrorHandler(this);
+			// this._oErrorHandler = new ErrorHandler(this);
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
@@ -63,11 +63,11 @@ sap.ui.define([
 		 * @public
 		 * @override
 		 */
-		destroy : function () {
-			this._oErrorHandler.destroy();
-			// call the base component's destroy function
-			UIComponent.prototype.destroy.apply(this, arguments);
-		},
+		// destroy : function () {
+		// 	this._oErrorHandler.destroy();
+		// 	// call the base component's destroy function
+		// 	UIComponent.prototype.destroy.apply(this, arguments);
+		// },
 
 		/**
 		 * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
@@ -75,21 +75,21 @@ sap.ui.define([
 		 * @public
 		 * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
 		 */
-		getContentDensityClass : function() {
-			if (this._sContentDensityClass === undefined) {
-				// check whether FLP has already set the content density class; do nothing in this case
-				// eslint-disable-next-line sap-no-proprietary-browser-api
-				if (document.body.classList.contains("sapUiSizeCozy") || document.body.classList.contains("sapUiSizeCompact")) {
-					this._sContentDensityClass = "";
-				} else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
-					this._sContentDensityClass = "sapUiSizeCompact";
-				} else {
-					// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
-					this._sContentDensityClass = "sapUiSizeCozy";
-				}
-			}
-			return this._sContentDensityClass;
-		}
+		// getContentDensityClass : function() {
+		// 	if (this._sContentDensityClass === undefined) {
+		// 		// check whether FLP has already set the content density class; do nothing in this case
+		// 		// eslint-disable-next-line sap-no-proprietary-browser-api
+		// 		if (document.body.classList.contains("sapUiSizeCozy") || document.body.classList.contains("sapUiSizeCompact")) {
+		// 			this._sContentDensityClass = "";
+		// 		} else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
+		// 			this._sContentDensityClass = "sapUiSizeCompact";
+		// 		} else {
+		// 			// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+		// 			this._sContentDensityClass = "sapUiSizeCozy";
+		// 		}
+		// 	}
+		// 	return this._sContentDensityClass;
+		// }
 
 	});
 
