92b978f360bb3b57b5eaa0584b0e4ab71a2e204e
A60951 <A6095@cnspartner.com>
2021-01-25 09:19:37 +0000

    view(공통방식)로 CRUD하도록 변경

--- app/dp/vi/basePriceArlMgt/webapp/controller/BasePriceList.controller.js
+++ app/dp/vi/basePriceArlMgt/webapp/controller/BasePriceList.controller.js
@@ -20,11 +20,11 @@ sap.ui.define([
             var sReturnValue = 1;
 
             if( sStautsCodeParam === "AR" ) {
-                sReturnValue = 5;
+                sReturnValue = 8;
             }else if( sStautsCodeParam === "AP" ) {
-                sReturnValue = 7;
+                sReturnValue = 9;
             }else if( sStautsCodeParam === "RJ" ) {
-                sReturnValue = 3;
+                sReturnValue = 2;
             }
 
             return sReturnValue;
@@ -111,31 +111,6 @@ sap.ui.define([
                     console.log("error", data);
                 }
             });
-        },
-
-        /**
-         * Date 데이터를 String 타입으로 변경. 예) 2020-10-10
-         */
-        _changeDateFormat: function (oDateParam, sGubun) {
-            var oDate = oDateParam || new Date(),
-                sGubun = sGubun || "",
-                iYear = oDate.getFullYear(),
-                iMonth = oDate.getMonth()+1,
-                iDate = oDate.getDate(),
-                iHours = oDate.getHours(),
-                iMinutes = oDate.getMinutes(),
-                iSeconds = oDate.getSeconds();
-
-            var sReturnValue = "" + iYear + sGubun + this._getPreZero(iMonth) + sGubun + this._getPreZero(iDate);
-
-            return sReturnValue;
-        },
-
-        /**
-         * 넘겨진 Parameter가 10이하이면 숫자앞에 0을 붙여서 return
-         */
-        _getPreZero: function (iDataParam) {
-            return (iDataParam<10 ? "0"+iDataParam : iDataParam);
         },
 
         /**
