7901fb2957b6ff1c8c823e4f0fd7ab684259af4b
A59965 <SM59965@cnspartner.com>
2021-02-01 08:35:24 +0000

    vpMgt edit 20210201 17:35

--- app/pg/vp/vpMgt/webapp/controller/MidObjectDetail.controller.js
+++ app/pg/vp/vpMgt/webapp/controller/MidObjectDetail.controller.js
@@ -789,7 +789,20 @@ sap.ui.define([
 			});
         },
 
+        chkReplaceChange: function (oEvent) {
+            console.log("livechange!!");
+            //var regex = /[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]/gi;     // 특수문자 제거 (한글 영어 숫자만)
+            //var regex = /[^a-zA-Z0-9\s ]/gi;                   // 특수문자 제거 (영어 숫자만)
+            var regex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g;  //한글 제거 11/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g%^&*()_+|<
 
+            var newValue = oEvent.getParameter("newValue");
+            //$(this).val(v.replace(regexp,''));
+            if (newValue !== "") {
+                newValue = newValue.replace(regex, "");
+                oEvent.oSource.setValue(null);
+                oEvent.oSource.setValue(newValue);
+            }
+        },
         _supplySearch: function(aFilter) {
 
 
