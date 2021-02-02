a66196442ddebf5816e51f4dc5eea7fa9e2d2e6a
a60264 <a60264oh@cnspartner.com>
2021-02-02 05:28:27 +0000

    PR Create - dialog

--- app/op/util/control/ui/CurrencyDialog.js
+++ app/op/util/control/ui/CurrencyDialog.js
@@ -14,38 +14,38 @@ sap.ui.define([
 ], function (Parent, Renderer, ODataV2ServiceProvider, Filter, FilterOperator, Sorter, GridData, VBox, Column, Label, Text, Input) {
     "use strict";
 
-    var WbsDialog = Parent.extend("cm.util.control.ui.WbsDialog", {
+    var CurrencyDialog = Parent.extend("cm.util.control.ui.CurrencyDialog", {
 
         metadata: {
             properties: {
                 aaa: { type: "string", group: "Appearance", defaultValue: "50em"},
                 contentWidth: { type: "string", group: "Appearance", defaultValue: "50em"},
-                keyField: { type: "string", group: "Misc", defaultValue: "wbs_code" },
-                textField: { type: "string", group: "Misc", defaultValue: "wbs_name" }
+                keyField: { type: "string", group: "Misc", defaultValue: "currency_code" },
+                textField: { type: "string", group: "Misc", defaultValue: "currency_code_name" }
             }
         },
 
         renderer: Renderer,
 
         createSearchFilters: function(){
-            this.oSearchWbsCode = new Input({ placeholder: this.getModel("I18N").getText("/WBS_CODE")});
-            this.oSearchWbsCode.attachEvent("change", this.loadData.bind(this));
+            this.oSearchCurrencyCode = new Input({ placeholder: this.getModel("I18N").getText("/CURRENCY_CODE")});
+            this.oSearchCurrencyCode.attachEvent("change", this.loadData.bind(this));
 
-            this.oSearchWbsName = new Input({ placeholder: this.getModel("I18N").getText("/WBS_NAME")});
-            this.oSearchWbsName.attachEvent("change", this.loadData.bind(this));
+            this.oSearchCurrencyName = new Input({ placeholder: this.getModel("I18N").getText("/CURRENCY_CODE_NAME")});
+            this.oSearchCurrencyName.attachEvent("change", this.loadData.bind(this));
             
             return [
                 new VBox({
                     items: [
-                        new Label({ text: this.getModel("I18N").getText("/WBS_CODE"), required: true}),
-                        this.oSearchWbsCode
+                        new Label({ text: this.getModel("I18N").getText("/CURRENCY_CODE"), required: true}),
+                        this.oSearchCurrencyCode
                     ],
                     layoutData: new GridData({ span: "XL2 L3 M5 S10"})
                 }),
                 new VBox({
                     items: [
-                        new Label({ text: this.getModel("I18N").getText("/WBS_NAME"), required: true}),
-                        this.oSearchWbsName
+                        new Label({ text: this.getModel("I18N").getText("/CURRENCY_CODE_NAME"), required: true}),
+                        this.oSearchCurrencyName
                     ],
                     layoutData: new GridData({ span: "XL2 L3 M5 S10"})
                 })
@@ -57,42 +57,42 @@ sap.ui.define([
                 new Column({
                     width: "30%",
                     hAlign: "Left",
-                    label: new Label({text: this.getModel("I18N").getText("/WBS_CODE")}),
-                    template: new Text({text: "{wbs_code}"})
+                    label: new Label({text: this.getModel("I18N").getText("/CURRENCY_CODE")}),
+                    template: new Text({text: "{currency_code}"})
                 }),
                 new Column({
-                    label: new Label({text: this.getModel("I18N").getText("/WBS_NAME")}),
-                    template: new Text({text: "{wbs_name}"})
+                    label: new Label({text: this.getModel("I18N").getText("/CURRENCY_CODE_NAME")}),
+                    template: new Text({text: "{currency_code_name}"})
                 })
             ];
         },
 
         loadData: function(){
-            var sWbsCode = this.oSearchWbsCode.getValue(),
-                sWbsName = this.oSearchWbsName.getValue(),
+            var sCurrencyCode = this.oSearchCurrencyCode.getValue(),
+                sCurrencyName = this.oSearchCurrencyName.getValue(),
                 oParam = this.getServiceParameters(),
                 aFilters = oParam.filters || [],
                 aSorters = oParam.sorters || [];
-            if(sWbsCode){
+            if(sCurrencyCode){
                 aFilters.push(new Filter({
-                                path: "wbs_code",
+                                path: "currency_code",
                                 operator: FilterOperator.Contains,
-                                value1: sWbsCode,
+                                value1: sCurrencyCode,
                                 caseSensitive: false
                             }));
             }
-            if(sWbsName){
+            if(sCurrencyName){
                 aFilters.push(new Filter({
-                                path: "wbs_name",
+                                path: "currency_code_name",
                                 operator: FilterOperator.Contains,
-                                value1: sWbsName,
+                                value1: sCurrencyName,
                                 caseSensitive: false
                             }));
             }
             
-            aSorters.push(new Sorter("wbs_code", false));
+            aSorters.push(new Sorter("currency_code", false));
             this.oDialog.setBusy(true);
-            ODataV2ServiceProvider.getServiceByUrl("srv-api/odata/v2/op.util.MstService/").read("/Wbs_Mst", {
+            ODataV2ServiceProvider.getServiceByUrl("srv-api/odata/v2/cm.util.CommonService/").read("/Currency", {
                 fetchOthers: true,  //TODO: 가능하면 fetchOthers 옵션을 false 하세요.
                 filters: aFilters,
                 sorters: aSorters,
@@ -115,5 +115,5 @@ sap.ui.define([
 
     });
 
-    return WbsDialog;
+    return CurrencyDialog;
 }, /* bExport= */ true);
