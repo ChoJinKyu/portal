104eb6e95abc9e04d9f8677cc343a854bf4324df
A60951 <A6095@cnspartner.com>
2021-02-08 09:57:14 +0000

    family material code 추가 필드 적용

--- app/dp/vi/basePriceProgressStatusMgt/webapp/controller/App.controller.js
+++ app/dp/vi/basePriceProgressStatusMgt/webapp/controller/App.controller.js
@@ -23,6 +23,33 @@ sap.ui.define([
 
         return sReturnValue;
     },
+
+        /**
+     * Date 데이터를 String 타입으로 변경. 예) 2020-10-10
+     */
+    _changeDateString: function (oDateParam, sGubun) {
+        var oDate = oDateParam || new Date(),
+            sGubun = sGubun || "",
+            iYear = oDate.getFullYear(),
+            iMonth = oDate.getMonth()+1,
+            iDate = oDate.getDate(),
+            iHours = oDate.getHours(),
+            iMinutes = oDate.getMinutes(),
+            iSeconds = oDate.getSeconds();
+
+        var sReturnValue = "" + iYear + sGubun + this._getPreZero(iMonth) + sGubun + this._getPreZero(iDate);
+
+        return sReturnValue;
+    },
+
+    /**
+     * 넘겨진 Parameter가 10이하이면 숫자앞에 0을 붙여서 return
+     */
+    _getPreZero: function (iDataParam) {
+        return (iDataParam<10 ? "0"+iDataParam : iDataParam);
+    },
+
+    
   });
 
 });
